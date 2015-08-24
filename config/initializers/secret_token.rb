f5599d04e2ccc4ee8c5bab7f6e4114519ade8b1a
vagrant <bonjias@gmail.com>
2015-08-24 03:14:45 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gps::Application.config.secret_key_base = 'ce6e1eecfe5d41eb1c35063c87e2d6fa7e23958a4f43c8437b87743e3fe06e5c3cd36e39febaaa8564f8e60c3849056dc3095b05de6fa9c302f1496cc84202ff'
